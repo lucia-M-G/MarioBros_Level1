cedd8f5b48d11694ab9f7ff1ef8ff0dfab936217
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2024-11-25 13:04:06 +0100

    Configuración de la pantalla

---
+++ Main.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+# Inicializar Pygame
+pygame.init()
+
+# Configuración de la pantalla
+ANCHO = 800
+ALTO = 400
+screen = pygame.display.set_mode((ANCHO, ALTO))
+pygame.display.set_caption("Mario Bros - Nivel 1")
